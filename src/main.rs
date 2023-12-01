f46b3421789a25c95828c3fcfea265efe6beacea
Quentin Boyer <quentin.boyer@eviden.com>
2023-12-01 23:09:49 +0100

    Basic cargo aoc tool

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Commands {
+    New,
+    Run,
+}
+
+fn main() -> color_eyre::Result<()> {
+    let args = Args::parse();
+    dbg!(args);
+
+    Ok(())
+}
